1d4e36651199f2d472b70b938e909ffb3ff532fb
Quentame <polletquentin74@me.com>
2023-10-24 07:32:54 +0000

    Uniform doc config

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,10 @@
 """Sphinx configuration."""
+from datetime import datetime
+
+
 project = "SFR Box API"
-author = "epenet"
-copyright = "2022, epenet"
+author = "HACF (created and maintained by @epenet)"
+copyright = f"{datetime.now().year}, {author}"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
