50d3db30d7b6296adf4a24c54d6ac28efee37f3d
epenet <6771947+epenet@users.noreply.github.com>
2023-11-07 08:11:21 +0100

    mypy

--- src/sfrbox_api/models.py
+++ src/sfrbox_api/models.py
@@ -7,8 +7,8 @@ try:
     from pydantic.v1 import validator
     from pydantic.v1.dataclasses import dataclass
 except ImportError:  # pragma: no cover
-    from pydantic import validator
-    from pydantic.dataclasses import dataclass
+    from pydantic import validator  # type: ignore[assignment]
+    from pydantic.dataclasses import dataclass  # type: ignore[no-redef]
 
 
 def _empty_to_none(v: Union[int, str, None]) -> Optional[Union[int, str, None]]:
