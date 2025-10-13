deff5f02e21ec8f09237c8678ce0c943c47ffd61
epenet <6771947+epenet@users.noreply.github.com>
2025-10-13 10:15:42 +0200

    Drop support for python 3.9

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,7 @@ except ImportError:
 
 
 package = "sfrbox_api"
-python_versions = ["3.11", "3.10", "3.9"]
+python_versions = ["3.11", "3.10"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
